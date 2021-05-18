6117ea1cf3d29868b7063d584c31f2fce7523f65
Ferdinand Ebert <ferdinand.ebert@lhind.dlh.de>
2021-05-18 11:43:57 +0200

    [general] Initial project setup

---
+++ src/main/java/de/lhind/calculator/CalculatorController.java
@@ -0,0 +1,12 @@
+package de.lhind.calculator;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+	@PostMapping
+	public void add() {
+	}
+}
